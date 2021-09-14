29d15149c030bbd89e9879fc1779434ef82d0d2d
Axel Olivas <axelbrolivas@gmail.com>
2021-09-14 09:26:19 -0700

    Solved helloWorld.js and repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num < 0){
+        return 'ERROR';
+    } else if(num === 0){
+        return '';
+    } else {
+        var newStr = '';
+        for(let i=0; i<num; i++){
+            newStr += str;
+        }
+        return newStr
+    }
 
 };
 
