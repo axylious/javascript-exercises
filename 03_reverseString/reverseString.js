6bc58a434c8535dcc443d4f54952b0ea41198ff7
Axel Olivas <axelbrolivas@gmail.com>
2021-09-14 09:54:52 -0700

    solved reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
 const reverseString = function(str) {
-
+    var arr = str.split('');
+    var rev = [];
+    for(let i=0; i<str.length; i++){
+        let x = arr[i];
+        rev.unshift(x);
+    }
+    return rev.join('');
 };
 
 // Do not edit below this line
